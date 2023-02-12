3bd36e935610bbdf850077d627d3a5f3edb9b3e7
Chenghao SHEN <chenghao.shen@gmail.com>
2023-02-12 20:12:53 +0800

    V1

---
+++ cn/precache-manifest.22babd9175a347d458523b4257fd1f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9738c028bb4b1bec93b45e89249d81a",
+    "url": "/cn/index.html"
+  },
+  {
+    "revision": "5bf70a5e492ebbbaa80c",
+    "url": "/cn/static/css/2.55ffcb95.chunk.css"
+  },
+  {
+    "revision": "c3fa5b546bb40c7412f3",
+    "url": "/cn/static/css/main.432eddbb.chunk.css"
+  },
+  {
+    "revision": "5bf70a5e492ebbbaa80c",
+    "url": "/cn/static/js/2.20b9020d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cn/static/js/2.20b9020d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fa5b546bb40c7412f3",
+    "url": "/cn/static/js/main.d5937b9c.chunk.js"
+  },
+  {
+    "revision": "d1c9ffbbd057f8e3be1d",
+    "url": "/cn/static/js/runtime-main.e102c7bd.js"
+  }
+]);
---
+++ precache-manifest.377d50ab383894c4e10d46b4eb101fa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8d37bf9954a6ed579665b6652f2ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929b8a360cc2e9fae145",
+    "url": "/static/css/2.55ffcb95.chunk.css"
+  },
+  {
+    "revision": "3f6cff2d96eed385c07f",
+    "url": "/static/css/main.432eddbb.chunk.css"
+  },
+  {
+    "revision": "929b8a360cc2e9fae145",
+    "url": "/static/js/2.18b68041.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.18b68041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6cff2d96eed385c07f",
+    "url": "/static/js/main.ebff12bf.chunk.js"
+  },
+  {
+    "revision": "d8186772801e57ae28d4",
+    "url": "/static/js/runtime-main.da5ef7e3.js"
+  }
+]);
